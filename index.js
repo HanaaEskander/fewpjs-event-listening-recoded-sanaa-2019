f057ce89debda7143ebf28d0ba632deddfadf5ff
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 20:07:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
-  alert('I was clicked!');
-});
-
+document.querySelector('main#main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "JALAL is the champion";
+document.body.appendChild(newHeader);
 
 addingEventListener('click');
