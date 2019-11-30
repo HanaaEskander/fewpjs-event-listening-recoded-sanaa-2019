0d449ad05cb2fe6cce1913f9287307091b76b8ae
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 20:05:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(event) {
+  let input = document.querySelector('input#input');
+  input.addEventListener(event,function(event) {
+  alert('I was clicked!');
+});
 }
+
+
+addingEventListener('click');
